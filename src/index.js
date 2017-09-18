ff3c774fc1c1fb9103d353fe02c737c188491677
Dan Ward <daniel.ward@forrent.com>
2017-09-18 17:59:08 -0400

    Add ability to jump to moves in history

--- src/index.js
+++ src/index.js
@@ -49,12 +49,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -65,15 +66,31 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
-      squares: squares,
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const description = move ? 'Move #' + move : 'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={ () => this.jumpTo(move) }>{description}</a>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -92,7 +109,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
